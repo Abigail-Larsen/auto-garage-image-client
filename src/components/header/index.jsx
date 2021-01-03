44fefeff66fa09123a290662fe50c7541686cbef
Abigail <abigailthelin@workfront.com>
2021-01-03 16:01:44 -0700

    feat: added header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-// import Modal from '@material-ui/core/Modal';
-// import Menu from '@material-ui/icons/Menu';
-// import SignIn from '../login/sign-in'
-// import SignUp from '../login/sign-up'
 
 export const Header = () => {
   const rootRef = React.useRef(null);
@@ -27,35 +23,38 @@ export const Header = () => {
 
     return (
       <>
-    <AppBar position="static" className={classes.root}>
-      <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <Menu />
-          </IconButton> */}
-          <Typography variant="h6" className={classes.title}>
-             {/* Image Assemble */}
-          </Typography>
-          <Button onClick={(e) => handleOpen('login')} color="inherit">Login</Button>
+        <AppBar position="static" className={classes.root}>
+            <Toolbar className={classes.toolbar}>
 
-          <Button onClick={() => handleOpen('signUp')} color="primary">Sign up</Button>
-      </Toolbar>
+                {/* <Link to='/'> */}
+                    <Typography variant="h7">
+                        Auto Garage Image
+                    </Typography>
+                {/* </Link> */}
 
-      {/* <Modal
-          open={open}
-          onClose={handleClose}
-          className={classes.modal}
-          container={() => rootRef.current}
-        >
-          <div className={classes.paper}>
-            {
-              modal === 'login' ?
-              <SignIn/> : 
-              <SignUp/>
-            } 
-          </div>
-        </Modal> */}
 
-    </AppBar>
+                <div>
+                    <Button onClick={(e) => handleOpen('login')} color="inherit">Login</Button>
+                    <Button onClick={() => handleOpen('signUp')} color="primary">Sign up</Button>
+                </div>
+            </Toolbar>
+
+            {/* <Modal
+                open={open}
+                onClose={handleClose}
+                className={classes.modal}
+                container={() => rootRef.current}
+                >
+                <div className={classes.paper}>
+                    {
+                    modal === 'login' ?
+                    <SignIn/> : 
+                    <SignUp/>
+                    } 
+                </div>
+                </Modal> */}
+
+        </AppBar>
       </>
       
     )
@@ -63,14 +62,12 @@ export const Header = () => {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
     backgroundColor: '#d1c4e9',
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
+  toolbar: {
+    display: 'flex',
+    justifyItems:'flex-start',
+    justifyContent: 'space-between',
   },
   paper: {
     position: 'absolute',
