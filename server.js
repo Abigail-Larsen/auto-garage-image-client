68b4ee04f20983aa87bdc92366805b88408af01c
Abigail <abigailthelin@workfront.com>
2021-03-20 16:34:34 -0600

    test

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  createProxyMiddleware({
-    target: 'https://node-service-graphql.herokuapp.com/',
-    changeOrigin: true,
-  })
-);
+// app.use(
+//   createProxyMiddleware({
+//     target: 'http://localhost:9000',
+//     changeOrigin: true,
+//   })
+// );
 
 app.use(express.static(path.join(__dirname, 'build')));
 
